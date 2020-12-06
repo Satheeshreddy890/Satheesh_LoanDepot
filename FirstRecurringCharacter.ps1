b26159a358aa253058ede7828e81dd49bb67a54f
Satheeshreddy890 <75584255+Satheeshreddy890@users.noreply.github.com>
2020-12-06 11:45:15 -0800

    Add files via upload
    Working Powershell code to return the First Recurring Character in the given String.

---
+++ FirstRecurringCharacter.ps1
@@ -0,0 +1,22 @@
+﻿function getFirstRecurringChar {
+    [CmdletBinding()]
+    param (
+    [string]$teststring
+    )
+    $temp = @{} #Temporary Hashtable to store each unique element
+    $testArray = $teststring.ToCharArray() #Converting the input string to an array of characters
+
+    #Iterating on the array and  checking if element read already exists in Hashtable
+    foreach ($char in $testArray) {
+        if($char -in $temp.Keys) {
+            $char
+            break
+        }
+        else {
+            $temp[$char] = $null
+        }
+    }
+}
+
+#Calling Function with the required Parameters
+getFirstRecurringChar("abcdedcba")
